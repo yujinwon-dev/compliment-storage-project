13969e30fbcb0129d04500a98af776d00b7ddbab
yujin <wwonyujin@gmail.com>
2022-06-30 21:23:33 +0900

    feat: 🎸 텍스트 감지 api 요청 구현

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -6,10 +6,50 @@ import { useAppDispatch } from '../store/hooks';
 import { add } from '../store/modules/compliment';
 
 export default function Form() {
-  const [name, setName] = useState('');
-  const [content, setContent] = useState('');
+  const [name, setName] = useState<string>('');
+  const [selectedFile, setSelectedFile] = useState<string | ArrayBuffer | null>();
+  const [content, setContent] = useState<string>('');
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  function handleImageSelection(event: React.ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target;
+    if (files) {
+      const reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onload = function() {
+        setSelectedFile(reader.result?.slice(23));
+      }
+    }
+  }
+
+  function handleTextDetection() {
+    const url = `https://vision.googleapis.com/v1/images:annotate?key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
+    if (!selectedFile) return;
+    const payload = {
+      requests: [
+        {
+          image: {
+              content: selectedFile,
+          },
+          features: [{
+              type: 'TEXT_DETECTION',
+          }],
+          imageContext: {
+            languageHints: ["ko"],
+          },
+        }
+      ]
+    };
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+    })
+      .then(res => {
+        console.log(res);
+      });
+  }
 
   function handleFormSubmit() {
     dispatch(add({name, content}));
@@ -40,7 +80,13 @@ export default function Form() {
         />
         <label htmlFor="content">내용</label>
         <button>클립보드 붙여넣기</button>
-        <button>사진에서 텍스트 추출하기</button>
+        <input
+          type="file"
+          name="input-file"
+          id="input-file"
+          onChange={e => handleImageSelection(e)}
+        />
+        <button type="button" onClick={handleTextDetection}>사진에서 텍스트 추출하기</button>
         <textarea
           name="content"
           id="content"
